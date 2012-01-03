e1b008fa1e298bd2d8cd139e69cf519aade4e589
xufeng <81959804@qq.com>
2012-01-03 22:07:51 +0800

    Table Node add_index

--- db/migrate/20120103133428_create_nodes.rb
+++ db/migrate/20120103133428_create_nodes.rb
@@ -1,10 +1,10 @@
 class CreateNodes < ActiveRecord::Migration
   def change
     create_table :nodes do |t|
-      t.string :name
-      t.references :section
-      t.integer :sort
-      t.integer :topics_count
+      t.string :name, :null => false
+      t.references :section, :null => false
+      t.integer :sort, :null => false, :default => 0
+      t.integer :topics_count, :null => false, :default => 0
       t.string :summary
 
       t.timestamps
