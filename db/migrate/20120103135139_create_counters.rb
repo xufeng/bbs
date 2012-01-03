242acc2cdce56396386eebff4bdcfa57c87aa4b3
xufeng <81959804@qq.com>
2012-01-03 22:16:36 +0800

    Table Reply and Counter field not null

--- db/migrate/20120103135139_create_counters.rb
+++ db/migrate/20120103135139_create_counters.rb
@@ -1,10 +1,12 @@
 class CreateCounters < ActiveRecord::Migration
   def change
     create_table :counters do |t|
-      t.string :key
-      t.string :value
+      t.string :key, :null => false
+      t.string :value, :null => false
 
       t.timestamps
     end
+
+    add_index :counters, :key
   end
 end
