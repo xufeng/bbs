e95f7a98ac68883b70719a2471ce24036df817b2
xufeng <81959804@qq.com>
2012-01-07 17:28:25 +0800

    user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  vaidates_presence_of :email, :name, :passwd
+  validates_presence_of :email, :name, :passwd
 
   #封面图
   has_attached_file :avatar,
@@ -9,8 +9,7 @@ class User < ActiveRecord::Base
       :normal => "48x48#",
       :large => "80>",
      },
-
-     :url => "#{APP_CONFIG{'upload_url'}}/:class/:attachment/:hashed_path/:id_:style.jpg",
-     :path => "#{APP_CONFIG{'upload_root'}}/:class/:attachment/:hashed_path/:id_:style.jpg",
+:url => "#{APP_CONFIG['upload_url']}/:class/:attachment/:hashed_path/:id_:style.jpg",
+:path => "#{APP_CONFIG['upload_root']}/:class/:attachment/:hashed_path/:id_:style.jpg",
      :default_url => "avatar/:style.jpg"
 end
---
+++ config/initializers/paperclip_hashpath.rb
@@ -0,0 +1,8 @@
+require "digest"
+
+Paperclip.interpolates :hashed_path do |attachment, style|
+    hash = Digest::MD5.hexdigest(attachment.instance.id.to_s)
+      hash_path = ''
+        3.times { hash_path += '/' + hash.slice!(0..2) }
+          hash_path[1..12]
+end
