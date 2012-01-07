53b3fb900563e8077556584f98aeb2ccdc803ff5
xufeng <81959804@qq.com>
2012-01-07 16:37:35 +0800

    change user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  vaidates_presence_of :email, :name, :passwd
+
+  #封面图
+  has_attached_file :avatar,
+    :default_style => :normal,
+    :styles => {
+      :small => "16x16#",
+      :normal => "48x48#",
+      :large => "80>",
+     },
+
+     :url => "#{APP_CONFIG{'upload_url'}}/:class/:attachment/:hashed_path/:id_:style.jpg",
+     :path => "#{APP_CONFIG{'upload_root'}}/:class/:attachment/:hashed_path/:id_:style.jpg",
+     :default_url => "avatar/:style.jpg"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120103135139) do
+ActiveRecord::Schema.define(:version => 20120107080953) do
 
   create_table "counters", :force => true do |t|
     t.string   "key",        :null => false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20120103135139) do
     t.string   "tagline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "verified"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
